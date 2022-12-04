01191ebb5932a3dbb9af20e507aed7a605d422ab
GXtal <zazaza00133@gmail.com>
2022-12-04 20:51:03 +0300

    preaparation
    installed roslyn and created empry fiiles that i will need

---
+++ TestGenerator/TestGeneratorLib/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace TestGeneratorLib
+{
+    public class Test
+    {
+        private SyntaxTree _finalCode;
+
+        public string GetCode()
+        {
+            return _finalCode.ToString();
+        }
+        
+    }
+}
