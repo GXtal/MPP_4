5047a4ceb0fdedbdbd50599b5151fd17051e6c77
GXtal <zazaza00133@gmail.com>
2022-12-07 16:38:20 +0300

    Added Tetst

---
+++ TestGenerator/TestGeneratorTests/TestGenerator.cs_TestGeneratorLib.GeneratedTests_TestGenerator.cs
@@ -0,0 +1,107 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using TestGeneratorLib;
+using NUnit.Framework;
+
+
+namespace TestGeneratorLib.GeneratedTests
+{
+    [TestFixture]
+    public class TestOfTestGenerator
+    {
+        [Test]
+        public async Task GenerateForFile_OneFileOneClass_Test()
+        {
+            //Arrange
+            TestGenerator generator = new TestGenerator();
+
+            string content;
+            string file = @"F:\5 sem\ñïï\test files\BoolGenerator.cs";
+
+            //act
+            using (var sr = new StreamReader(file))
+            {
+                content = sr.ReadToEnd();
+            }
+
+            var tests = await generator.GenerateForFile(content);
+
+            //assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(tests, Is.Not.EqualTo(null));
+                Assert.That(tests.Count, Is.EqualTo(1));
+                Assert.That(tests[0].ClassName, Is.EqualTo("BoolGenerator"));
+            });
+            
+        }
+
+        [Test]
+        public async Task GenerateForFile_OneFileTwoClass_Test()
+        {
+            //Arrange
+            TestGenerator generator = new TestGenerator();
+
+            string content;
+            string file = @"F:\5 sem\ñïï\test files\MultiFile.cs";
+
+            //act
+            using (var sr = new StreamReader(file))
+            {
+                content = sr.ReadToEnd();
+            }
+
+            var tests = await generator.GenerateForFile(content);
+
+            //assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(tests, Is.Not.EqualTo(null));
+                Assert.That(tests.Count, Is.EqualTo(2));
+                Assert.That(tests[0].ClassName, Is.EqualTo("BoolGenerator"));
+                Assert.That(tests[1].ClassName, Is.EqualTo("CharGenerator"));
+            });
+
+        }
+
+        [Test]
+        public async Task CheckTestsSructure_Test()
+        {
+            //Arrange
+            TestGenerator generator = new TestGenerator();
+
+            string content;
+            string file = @"F:\5 sem\ñïï\test files\BoolGenerator.cs";
+
+            //act
+            using (var sr = new StreamReader(file))
+            {
+                content = sr.ReadToEnd();
+            }
+
+            var tests = await generator.GenerateForFile(content);
+            string res=tests[0].GetCode();
+
+            var root = CSharpSyntaxTree.ParseText(res).GetCompilationUnitRoot();
+
+            var classes = root.DescendantNodes().OfType<ClassDeclarationSyntax>()
+                .Where(_class => _class.Modifiers.Any(SyntaxKind.PublicKeyword));
+            var classDeclaration = classes.ElementAt(0);
+
+            var sourceMethods = classDeclaration.DescendantNodes().OfType<MethodDeclarationSyntax>()
+                .Where(sourceMethod => sourceMethod.Modifiers.Any(SyntaxKind.PublicKeyword)).ToList();
+
+            //assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(classDeclaration.AttributeLists.ElementAt(0).ToString(), Is.EqualTo("[TestFixture]"));
+                Assert.That(sourceMethods.Count, Is.EqualTo(2));
+
+                Assert.That(sourceMethods[0].AttributeLists.ElementAt(0).ToString(),Is.EqualTo("[Test]"));
+            });
+
+        }
+
+    }
+}
